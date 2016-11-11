68e39b23aae5433e41de757836627e1eda817c1b
dvchava <dvchava@DAVID>
2016-11-11 11:41:36 +0000

    commit Server1

---
+++ PD_Serv/src/pd_serv/Server.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pd_serv;
+
+import java.net.DatagramSocket;
+
+/**
+ *
+ * @author dvchava
+ */
+public class Server {
+    DatagramSocket hbSocket; //Heart beat Socket
+    String name;
+    String dirServIP;
+    int dirServPort;
+
+    public Server(DatagramSocket hbSocket, String nome, String dirServIP, int dirServPort) {
+        this.hbSocket = hbSocket;
+        this.name = nome;
+        this.dirServIP = dirServIP;
+        this.dirServPort = dirServPort;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DatagramSocket getHbSocket() {
+        return hbSocket;
+    }
+
+    public String getDirServIP() {
+        return dirServIP;
+    }
+
+    public void setDirServIP(String dirServIP) {
+        this.dirServIP = dirServIP;
+    }
+
+    public int getDirServPort() {
+        return dirServPort;
+    }
+
+    public void setDirServPort(int dirServPort) {
+        this.dirServPort = dirServPort;
+    }
+    
+    
+}
