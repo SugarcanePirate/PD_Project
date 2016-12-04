fd1b68d3939dc6d39cf392498042af0aee2c86e7
dvchava <dvchava@DAVID>
2016-12-04 16:17:21 +0000

    commit client - added UI / register in server

--- PD_Client/src/pd_client/PD_Client.java
+++ PD_Client/src/pd_client/PD_Client.java
@@ -5,52 +5,30 @@
  */
 package pd_client;
 
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.util.Arrays;
-
 /**
  *
  * @author David
  */
 public class PD_Client {
-    
-    public final static void clearConsole() {
-        char c = '\n';
-        int length = 25;
-        char[] chars = new char[length];
-        Arrays.fill(chars, c);
-        System.out.print(String.valueOf(chars));
-    }
-    
-    public static void printServerList(String[] list){
-        for(int i = 0; i < list.length; i++){
-            System.out.println(list[i]);
-        }
-    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String dirServIP;
+        String dirServIp;
         int dirServPort;
-        DatagramSocket connSocket = null;
-        
-        dirServIP = args[0];
-        dirServPort = Integer.parseInt(args[1]);
-        
-        try {
-            connSocket = new DatagramSocket(dirServPort);
-        } catch (SocketException e) {
-            System.err.println("Error creating the heart beat socket - " + e);
+
+        int argc = args.length;
+        if (argc != 2) {
+            System.out.println("Error - Number of arguments...");
             return;
         }
-        
-        Client client = new Client(connSocket, dirServIP, dirServPort);
-        
-        String[] serverList = client.connect();
-        clearConsole();
-        printServerList(serverList);
-}
+
+        dirServIp = args[0];
+        dirServPort = Integer.parseInt(args[1]);
+
+        ClientUI ui = new ClientUI(dirServIp, dirServPort);
+        ui.runUI();
+
+    }
 }
